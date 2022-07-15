9820cb1437cbf5d587582b247bf9debd2c069704
Guillaume Lacourt <guillaume.lacourt@arneogroup.com>
2022-07-15 11:26:00 +0200

    Add pagination feature

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -18,6 +18,15 @@ class Configuration implements ConfigurationInterface
 
         $rootNode
             ->children()
+                ->booleanNode('pagination')
+                    ->info('Enable or disable the pagination')
+                    ->defaultFalse()
+                ->end()
+                ->integerNode('page_size')
+                    ->info('Set the size of the pagination')
+                    ->defaultValue(100)
+                    ->min(1)
+                ->end()
                 ->arrayNode('mapping')
                     ->info('Set the mapping with entities, properties and anonymizer to be used.')
                     ->normalizeKeys(false)
